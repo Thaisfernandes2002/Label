fb9dfc647dac1c978d20f660257dce98022b953c
Thaisfernandes2002 <60987574+Thaisfernandes2002@users.noreply.github.com>
2020-03-04 18:52:22 -0300

    Add files via upload

---
+++ label1/Label1.java
@@ -0,0 +1,16 @@
+package label1;
+
+import javax.swing.JFrame;
+
+
+public class Label1 {
+    
+    public static void main(String[] args) {
+        LabelFrame labelFrame = new LabelFrame();
+        labelFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        labelFrame.setSize( 260, 180 );
+        labelFrame.setVisible( true );
+    }
+}
+        
+    
